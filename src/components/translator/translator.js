7085f396196b5163b083f8750ec343d84fc3b8e7
unknown <Anatoly>
2021-10-21 16:53:56 +0300

    first message

--- src/components/translator/translator.js
+++ src/components/translator/translator.js
@@ -11,9 +11,7 @@ export const translate = () => {
           textType: 'plain'
         },
         headers: {
-          'content-type': 'application/json',
-          'x-rapidapi-host': 'microsoft-translator-text.p.rapidapi.com',
-          'x-rapidapi-key': 'adcd717d2dmsh7f13be40e4a23c9p16a87ejsn910b67652bb7'
+       
         },
         data: [{Text: 'I would really like to drive your car around the block a few times.'}]
       };
